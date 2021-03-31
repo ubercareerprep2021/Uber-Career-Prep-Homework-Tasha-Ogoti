3375c1ea7250a552425d6da01e7fe4af7cade3a7
Tasha Ogoti <tsofyx@mail.missouri.edu>
2021-03-30 19:25:50 -0500

    Create  Part2.java

---
+++ Assignment-1/Part2.java
@@ -0,0 +1,38 @@
+/*
+  Receiving two strings
+  Assumptions:  1) Function is case-sensitive
+                2) Numbers can be present
+        
+  Steps
+  - Check to see whether the strings are of the same length
+  - Loop through checking whether the second string has identical characters to the first
+*/
+
+
+
+public boolean isStringPermutation(String s1, String s2){
+    int lengthS1 = s1.length(); //length of first string
+    int lengthS2 = s2.length();
+  
+    s1.toLower();// Convert string to lower-case
+    s2.toLower();// Convert string to lower-case
+  
+    //Length of strings check
+    if(lengthS1 != lengthS2){
+          return false;
+    }
+    
+    int i; //Used in looping through the string
+    for(i=0; i < lengthS1; i++){
+        String characterS2 = Character.toString(s2.charAt(i));
+        //Check to see if the character is present or not
+        if(s1.contains(characterS2)){
+            continue;
+        }
+        else if(!s1.contains(characterS2)){
+            return false;
+        }
+    }
+  
+    return true;
+}
