0ca1d4713504543361f50837bfb37f2480f6941c
Tasha Ogoti <tsofyx@mail.missouri.edu>
2021-09-08 21:19:39 -0500

    Create pow.java

---
+++ Assignment-3/Part3/pow.java
@@ -0,0 +1,18 @@
+
+public static double pow(double base, int power){
+    double total = 1.0;
+    boolean negativePower = (power < 0) ? true : false;
+
+    if(power == 0){
+      return 1.0;
+    }
+  
+    power = Math.abs(power);
+  
+    while(power != 0){
+      total = total * base;
+      power--;
+    }
+    
+    return (negativePower) ? (1/total) : total;
+ } 
