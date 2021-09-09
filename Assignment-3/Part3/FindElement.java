e742c0fc49a2b377e1634662acba0cad6aa7c983
Tasha Ogoti <tsofyx@mail.missouri.edu>
2021-09-08 21:01:21 -0500

    Create FindElement.java

---
+++ Assignment-3/Part3/FindElement.java
@@ -0,0 +1,52 @@
+   
+/*   Input: int array and  int value
+     Output: boolean
+     
+     Examples:   {4,5,6,1,2,3} , 5
+                 true
+                 
+                 {-2,-3,-4,-5,0,-1}, 2
+                 false
+                 
+     Time complexity: O(log N)
+     Space complexity: O(1)
+*/
+
+public static boolean findElement(int[] arr, int target){
+    if(arr.isEmpty()){
+        return false; 
+    }
+
+    int limit = arr.length - 1;
+  
+    if(limit == 0){
+      return (arr[0] == target) ? true : false;
+    }
+  
+    return recursiveBinary(arr,0,limit,target);
+ }
+ 
+public static boolean recursiveBinary(int[] arr, int start, int end, int target){
+    if(arr[start] == target){
+      return true;
+    }
+  
+    else if(arr[end] == target){
+      return true;
+    }
+  
+    else if((start+1) == end){
+      return false;
+    }
+  
+    //else if(start < end){
+    int mid = start + (end - start)/2;
+
+    if(arr[mid] == target){
+      return true;
+    }
+    else if(target > arr[mid+1] && target < arr[end]){
+      return recursiveBinary(arr, mid+1, end, target);
+    }
+    return recursiveBinary(arr, start, mid, target);
+}
